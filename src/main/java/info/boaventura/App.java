baf9f23aecd4a6e0c314568c94af5cc4a6738cd7
Valtoni Boaventura <valtoni@gmail.com>
2017-10-06 11:44:13 -0300

    Initial commit

---
+++ src/main/java/info/boaventura/App.java
@@ -0,0 +1,13 @@
+package info.boaventura;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
